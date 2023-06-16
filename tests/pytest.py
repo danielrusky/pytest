2ba855b98d453069af58092e31476226d2c521c7
Данила <spook9400@mail.ru>
2023-06-16 12:04:38 +0300

    testing get

---
+++ tests/pytest.py
@@ -0,0 +1,17 @@
+from utils import arrs
+
+
+def testing_get():
+    assert arrs.get([1, 2, 3], 0, "test") == 1
+    assert arrs.get([1, 2, 3], 1, "test") == 2
+    assert arrs.get([1, 2, 3], 2, "test") == 3
+    assert arrs.get([1, 2, 3], 10, "test") == "None"
+    assert arrs.get([], 0, "test") == "None"
+
+
+def testing_slice():
+    assert arrs.my_slice([], 1, 3) == []
+    'assert arrs.my_slice([1, 2, 3, 4], -1, 3) == [0, 3]'
+    'assert arrs.my_slice([1, 2, 3, 4], -5, 3) == [-1, 3]'
+    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
+    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
